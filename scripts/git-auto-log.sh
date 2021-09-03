a98b355023abe87545c693588cabcaa848d9901c
Fabian Sandoval Saldias <fabianvss@gmail.com>
2021-09-03 18:25:11 +0200

    Add git-auto-status

--- scripts/git-auto-log.sh
+++ scripts/git-auto-log.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # requires entr
 #   sudo apt install entr
 
+export GIT_OPTIONAL_LOCKS=0
+
 root=$(git rev-parse --show-toplevel)
 while true; do
     find "$root"/.git/{HEAD,refs/} | entr -cd ~/.biberconf/scripts/git-log-reversed-graph.sh "$@" || true
---
+++ scripts/git-auto-status.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $# -gt 0 ]]; then
+    seconds=$1
+    shift
+else
+    seconds=5
+fi
+
+root=$(git rev-parse --show-toplevel)
+while true; do
+    clear
+    git --no-optional-locks status "$@"
+    sleep $seconds
+done
