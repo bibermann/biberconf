72f7b9fd2231ac8534da837ec4a41eb5b7924ee9
Fabian Sandoval Saldias <fabianvss@gmail.com>
2024-01-08 18:43:18 +0100

    Cache system tests

--- .biberconf/scripts/system.sh
+++ .biberconf/scripts/system.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 is_debian() {
-    [ -f "/etc/debian_version" ] \
-        || grep -q -Ei 'debian|buntu|mint' /etc/issue \
-        || grep -q -Ei '^ID(_LIKE)=.*(debian|buntu|mint)' /etc/*release \
-        || grep -q -Ei '^ID(_LIKE)=.*(debian|buntu|mint)' /usr/lib/*release
+  if [ -z ${is_debian_exit_code+x} ]; then
+    [ -f /etc/debian_version ] \
+      || grep -q -Ei 'debian|buntu|mint' /etc/issue \
+      || grep -q -Ei '^ID(_LIKE)=.*(debian|buntu|mint)' /etc/*release \
+      || grep -q -Ei '^ID(_LIKE)=.*(debian|buntu|mint)' /usr/lib/*release
+    is_debian_exit_code=$?
+  fi
+  return "$is_debian_exit_code"
 }
 
 is_arch() {
+  if [ -z ${is_arch_exit_code+x} ]; then
     [ -f /etc/arch-release ] \
-        || grep -q -Ei 'arch' /etc/issue \
-        || grep -q -Ei '^ID(_LIKE)=.*arch' /etc/*release \
-        || grep -q -Ei '^ID(_LIKE)=.*arch' /usr/lib/*release
+      || grep -q -Ei 'arch' /etc/issue \
+      || grep -q -Ei '^ID(_LIKE)=.*arch' /etc/*release \
+      || grep -q -Ei '^ID(_LIKE)=.*arch' /usr/lib/*release
+    is_arch_exit_code=$?
+  fi
+  return "$is_arch_exit_code"
 }
 
 is_gui() {
