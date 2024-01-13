9f9790cdfb9ce23ca3dfeaa2487879a387bc48b5
Fabian Sandoval Saldias <fabianvss@gmail.com>
2024-01-13 18:41:57 +0100

    Add .gitignore

--- .biberconf/scripts/common.sh
+++ .biberconf/scripts/common.sh
@@ -12,49 +12,58 @@ COLOR_NO='\033[0m'
 BIBERCONF_TITLE="${COLOR_TITLE}=========\nBiberconf\n=========${COLOR_NO}"
 
 echo_success() {
-    echo
-    echo -e "${BIBERCONF_TITLE}${COLOR_SUCCESS}"
-    echo -e "$1"
-    shift
-    echo -en "${COLOR_INFO}"
-    for line in "$@"; do
-        echo -e "$line"
-    done
-    echo -e "${COLOR_NO}"
+  echo
+  echo -e "${BIBERCONF_TITLE}${COLOR_SUCCESS}"
+  echo -e "$1"
+  shift
+  echo -en "${COLOR_INFO}"
+  for line in "$@"; do
+    echo -e "$line"
+  done
+  echo -e "${COLOR_NO}"
 }
 
 exit_error() {
-    echo
-    echo -e "${BIBERCONF_TITLE}${COLOR_ERROR}"
-    1>&2 echo -e "$1"
-    shift
-    echo -en "${COLOR_INFO}"
-    for line in "$@"; do
-        1>&2 echo -e "$line"
-    done
-    echo -e "${COLOR_NO}"
-    exit 1
+  echo
+  echo -e "${BIBERCONF_TITLE}${COLOR_ERROR}"
+  echo 1>&2 -e "$1"
+  shift
+  echo -en "${COLOR_INFO}"
+  for line in "$@"; do
+    echo 1>&2 -e "$line"
+  done
+  echo -e "${COLOR_NO}"
+  exit 1
 }
 
 echo_info() {
-    echo
-    echo -e "${BIBERCONF_TITLE}${COLOR_INFO}"
-    for line in "$@"; do
-        echo -e "$line"
-    done
-    echo -e "${COLOR_NO}"
+  echo
+  echo -e "${BIBERCONF_TITLE}${COLOR_INFO}"
+  for line in "$@"; do
+    echo -e "$line"
+  done
+  echo -e "${COLOR_NO}"
 }
 
 read_reply() {
-    echo
-    echo -e "${BIBERCONF_TITLE}"
-    local array; array=("$@")
-    local prompt; prompt="${array[-1]}"
-    echo -en "${COLOR_INFO}"
-    for line in "${@:1:$#-1}"; do
-        echo -e "$line"
-    done
-    echo -en "${COLOR_PROMPT}$prompt${COLOR_NO}"
-    read -rp " "
-    echo -e "${COLOR_NO}"
+  echo
+  echo -e "${BIBERCONF_TITLE}"
+  local array
+  array=("$@")
+  local prompt
+  prompt="${array[-1]}"
+  echo -en "${COLOR_INFO}"
+  for line in "${@:1:$#-1}"; do
+    echo -e "$line"
+  done
+  echo -en "${COLOR_PROMPT}$prompt${COLOR_NO}"
+  read -rp " "
+  echo -e "${COLOR_NO}"
+}
+
+# https://stackoverflow.com/a/29613573
+quote_re() { sed -e 's/[^^]/[&]/g; s/\^/\\^/g; $!a\'$'\n''\\n' <<<"$1" | tr -d '\n'; }
+quote_subst() {
+  IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
+  printf %s "${REPLY%$'\n'}"
 }
